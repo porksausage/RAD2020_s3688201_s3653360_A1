483ccb2d5432a579809bc745f4a1949666d795c9
jiecai ma <s3653360@student.rmit.edu.au>
2020-05-25 18:08:41 +1000

    fix login

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-    before_action :logged_in_user, only: [:index, :new, :create, :destroy]
+    before_action :logged_in_user, only: [ :new, :create, :destroy]
     
     def show
       @post = Post.find(params[:id])
