dbae9b524a0cbcfcc0688b955627f0e5435cf7ed
Sifeng <wangsifeng0420@gmail.com>
2020-05-31 11:39:09 +0000

    guapi

--- db/migrate/20200429070736_create_users.rb
+++ db/migrate/20200429070736_create_users.rb
@@ -1,8 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :city
       t.string :mobile
       t.string :email
 
